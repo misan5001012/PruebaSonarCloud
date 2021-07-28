f216d46a8793ec4fbe11c499594467b0c031272e
Milena Sandoval Jimenez <milenaraquel.sandovaljimenez187@gmail.com>
2021-07-28 10:06:30 -0500

    commit inicial

---
+++ WinFormsApp1DescargarDocURL/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1DescargarDocURL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            string strURLFile = "https://s3.amazonaws.com/repositoriosii/49/mreg/20210601/524068.pdf?X-Amz-Content-Sha256=e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIIZPAUVWMH32PSKA%2F20210623%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20210623T135735Z&X-Amz-SignedHeaders=Host&X-Amz-Expires=10800&X-Amz-Signature=a03249c9bca91f2bd3e36d9b5f06e768bcb5c953e359f40ce8ff87a49ca59e2d";
+            string strPathToSave = @"E:\Digitex\Temporal";
+            downloadFileToSpecificPath(strURLFile, strPathToSave);
+        }
+
+
+        /// <summary>
+        /// Método que descarga un archivo de Internet en la ruta indicada.
+        /// </summary>
+        /// <param name="strURLFile">URL del archivo que se desea descargar</param>
+        /// <param name="strPathToSave">Ruta donde se desea almacenar el archivo</param>
+        public static void downloadFileToSpecificPath(string strURLFile, string strPathToSave)
+        {
+            // Se encierra el código dentro de un bloque try-catch.
+            try
+            {
+                // Se valida que la URL no esté en blanco.
+                if (String.IsNullOrEmpty(strURLFile))
+                {
+                    // Se retorna un mensaje de error al usuario.
+                    throw new ArgumentNullException("La dirección URL del documento es nula o se encuentra en blanco.");
+                }// Fin del if que valida que la URL no esté en blanco.
+
+                // Se valida que la ruta física no esté en blanco.
+                if (String.IsNullOrEmpty(strPathToSave))
+                {
+                    // Se retorna un mensaje de error al usuario.
+                    throw new ArgumentNullException("La ruta para almacenar el documento es nula o se encuentra en blanco.");
+                }// Fin del if que valida que la ruta física no esté en blanco.
+
+                // Se descargar el archivo indicado en la ruta específicada.
+                using (System.Net.WebClient client = new System.Net.WebClient())
+                {
+                    client.DownloadFile(strURLFile, strPathToSave);
+                }// Fin del using para descargar archivos.
+            }// Fin del try.
+            catch (Exception ex)
+            {
+                // Se retorna la excepción al cliente.
+                throw ex;
+            }// Fin del catch.
+        }// Fin del método downloadFileToSpecificPath.
+
+
+    }
+
+   
+}
